c33443aedd3d483c6b6bbc1d7c4559bbb45878aa
Tran Văn Den <tranvanden@gmail.com>
2021-12-29 14:21:13 +0700

    comment create file

--- pages/api/token/index.ts
+++ pages/api/token/index.ts
@@ -1,6 +1,6 @@
 
 import { NextApiRequest, NextApiResponse } from "next"
-import { readFileSync, writeFileSync } from 'fs';
+import { writeFileSync } from 'fs';
 
 const index = (req: NextApiRequest, res: NextApiResponse)=>{
    
@@ -8,7 +8,7 @@ if(req.method === 'POST'){
     
     const {token_type, expires_in, access_token, scope, refresh_token}=req.body
     const data = "token_type: "+ token_type +", expires_in:" + expires_in +", access_token: "+ access_token +", scope: "+scope+", refresh_token:"+ refresh_token
-    writeFileSync('./accesstoken.json',data)
+    //writeFileSync('./accesstoken.json',data)
     res.status(200).json({token_type: token_type, expires_in: expires_in, access_token:access_token, scope:scope, refresh_token: refresh_token})
 }else{
     res.status(200).json({message: "Không dúng với tiêu chi của abcd"})
